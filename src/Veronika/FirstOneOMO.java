973661d612ef303d538f1ce0bd0d8582bd633ccd
vpmc1 <vmccann1128@gmail.com>
2023-01-13 22:45:35 -0500

    another test one

---
+++ src/Veronika/FirstOneOMO.java
@@ -0,0 +1,9 @@
+package Veronika;
+
+public class FirstOneOMO {
+    public static void main(String[] args) {
+        String omo = "on my own";
+        System.out.println("I did it " + omo + "!");
+    }
+}
+//YAY!
