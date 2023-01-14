e00285f026df319b35f63436aba5b57404f42117
sayfulya <sayfulya@gmail.com>
2023-01-14 13:43:31 -0500

    TC105 created

---
+++ src/Elena/TC105_Elena.java
@@ -0,0 +1,14 @@
+package Elena;
+
+public class TC105_Elena {
+    public static void main(String[] args) {
+
+
+        StringBuilder sb = new StringBuilder("JAVA");
+
+        String str = new String("JAVA");
+
+        System.out.println(str.equals(sb));
+
+    }
+}
