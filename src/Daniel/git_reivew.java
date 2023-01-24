9275e58167ed08bc1312eaa82dca16a11676e7a9
Daraia <114092028+Daraia@users.noreply.github.com>
2023-01-23 20:08:02 -0600

    git review with Zuhal

---
+++ src/Daniel/git_reivew.java
@@ -0,0 +1,21 @@
+package Daniel;
+
+public class git_reivew {
+
+    public static void main(String[] args) {
+
+        System.out.println("Git review");
+        System.out.println("Git commands");
+        System.out.println("git init");
+        System.out.println("git add");
+        System.out.println("git comit-m \"updated xyz file\"");
+        System.out.println("git push");
+        System.out.println("git branch (feature)");
+        System.out.println("git checkout feature");
+        System.out.println("git status master feature");
+        System.out.println("git branch -d feature");
+        System.out.println("git pull");
+        System.out.println("git checkout feature");
+        System.out.println("git merge main");
+    }
+}
